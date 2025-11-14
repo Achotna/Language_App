e77b8a48debb2a0283184193a0f5d0f37ce34060
Achotna <achotna@gmail.com>
2025-11-14 21:58:14 +0100

    drop and drag files

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask_dropzone import Dropzone
 
 app = Flask(__name__)
+app.config.update(
+    UPLOAD_FOLDER="uploads/",
+    #only excel files DROPZONE_ALLOWED_FILE_TYPE="xls,xlsx", 
+    DROPZONE_MAX_FILE_SIZE=1024,  # MB
+)
 
-@app.route("/")
+dropzone = Dropzone(app)
+
+@app.route("/",methods=["GET", "POST"])
 def home():
-    return "<p>This is a Language App</p>"
+    if request.method == "POST":
+        f = request.files.get("file")
+        f.save(f"{app.config['UPLOAD_FOLDER']}/{f.filename}")
+
+
+    return render_template('index.html')
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
 #$env:FLASK_APP = "main.py"
