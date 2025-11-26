b71bc7fb745a1b1750a38f0cd4b71bd39fe4edfb
Achotna <achotna@gmail.com>
2025-11-26 11:52:19 +0100

    path audio anto

--- tts_test.py
+++ tts_test.py
@@ -2,4 +2,6 @@ from gtts import gTTS
 text = "错过你，尝尽酸甜都无味"
 language = 'zh-CN'
 tts = gTTS(text=text, lang=language)
-tts.save('C:\\Users\\Eleve\\Downloads\\Language app\\uploads\\test3_tts.mp3')
+tts.save('test_tts.mp3')
+
+#ordi anto : tts.save('C:\\Users\\Eleve\\Downloads\\Language app\\uploads\\test3_tts.mp3')
