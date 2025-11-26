6885bc8e9cc8f125bfe346721f4106b94281fbb9
Achotna <achotna@gmail.com>
2025-11-26 11:50:10 +0100

    path for audio

--- tts_test.py
+++ tts_test.py
@@ -2,4 +2,4 @@ from gtts import gTTS
 text = "错过你，尝尽酸甜都无味"
 language = 'zh-CN'
 tts = gTTS(text=text, lang=language)
-tts.save("test_tts.mp3")
+tts.save('C:\\Users\\Eleve\\Downloads\\Language app\\uploads\\test3_tts.mp3')
