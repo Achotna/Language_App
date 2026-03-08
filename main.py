45e7eae412f2a54bceb9b824041d4f114fa46b78
Achotna <achotna@gmail.com>
2026-03-08 10:52:18 +0100

    update

--- main.py
+++ main.py
@@ -4,9 +4,17 @@ import pandas as pd
 from sqlalchemy import create_engine
 import sqlite3
 #import tts_zoe_code
+import os
+from google.cloud import texttospeech
+from pydub import AudioSegment
+from pydub.generators import Sine
 
 
-#SETUP
+
+# ============================#
+#            SETUP            #
+# ============================#
+
 app = Flask(__name__)
 app.config.update(
     UPLOAD_FOLDER="uploads/",
@@ -14,18 +22,34 @@ app.config.update(
     DROPZONE_MAX_FILE_SIZE=1024,  # MB
 )
 dropzone = Dropzone(app)
+os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
 
-###################################################################################################
-#ZOE
 
-import os
-import pandas as pd
-from sqlalchemy import create_engine
-from google.cloud import texttospeech
-from pydub import AudioSegment
-import sqlite3
-from pydub.generators import Sine
 
+######################################## ============================ ################################################################
+########################################             ZOE              ################################################################
+######################################## ============================ ################################################################
+
+
+# Dictionnaire des voix#############
+VOICES = {
+    "cmn-CN": {
+        "female": "cmn-TW-Standard-A",
+        "male": "cmn-CN-Wavenet-B"
+    },
+    "en-GB": {
+        "female": "en-GB-Chirp3-HD-Leda",
+        "male": "en-GB-Chirp3-HD-Alnilam"
+    },
+    "fr-FR": {
+        "female": "fr-FR-Chirp-HD-O",
+        "male": "fr-FR-Chirp3-HD-Algenib"
+    },
+    "es-ES": {
+        "female": "es-ES-Chirp-HD-F",
+        "male": "es-ES-Chirp-HD-D"
+    }
+}
 
 
 #Google Text to Speech API setup
@@ -33,9 +57,8 @@ from pydub.generators import Sine
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "D:\\Projects\\Language app\\tts_service_account.json"
 client = texttospeech.TextToSpeechClient()
 
-# ============================
+#
 # Dossiers pour stockage audio
-# ============================
 BASE_DIR = "audio" 
 WORDS_DIR = f"{BASE_DIR}/words"
 TRANS_DIR = f"{BASE_DIR}/translations"
@@ -46,10 +69,7 @@ for d in [WORDS_DIR, TRANS_DIR, SILENCE_DIR, FINAL_DIR]:
     os.makedirs(d, exist_ok=True) #pour ne pas avoir d'erreur si le dossier existe déjà 
 
 
-# ======================================
 # Fonction générique Text-to-Speech
-# ======================================
-
 def text_to_speech(
     text: str,
     output_file: str,
@@ -80,34 +100,7 @@ def text_to_speech(
         out.write(response.audio_content)
 
 
-# ============================
-# Dictionnaire des voix
-# ============================
-
-VOICES = {
-    "cmn-CN": {
-        "female": "cmn-TW-Standard-A",
-        "male": "cmn-CN-Wavenet-B"
-    },
-    "en-GB": {
-        "female": "en-GB-Chirp3-HD-Leda",
-        "male": "en-GB-Chirp3-HD-Alnilam"
-    },
-    "fr-FR": {
-        "female": "fr-FR-Chirp-HD-O",
-        "male": "fr-FR-Chirp3-HD-Algenib"
-    },
-    "es-ES": {
-        "female": "es-ES-Chirp-HD-F",
-        "male": "es-ES-Chirp-HD-D"
-    }
-}
-
-
-# ============================
 # Génération du silence
-# ============================
-
 def generate_silence(duration_seconds: float) -> str:
     filename = f"{SILENCE_DIR}/{duration_seconds:.1f}s.wav"
 
@@ -122,23 +115,19 @@ def generate_silence(duration_seconds: float) -> str:
     return filename
 
 
-# ============================
+
 # Assemblage des audios
-# ============================
-
-def concatenate_audios(audio_files, output_file):
+def concatenate_audios(audio_files, output_file, num_loops):
     final_audio = AudioSegment.empty()
-
+     
+    
     for file in audio_files:
         final_audio += AudioSegment.from_wav(file)
 
     final_audio.export(output_file, format="wav")
 
 
-# ============================
 # Génération audio pour une entrée
-# ============================
-
 def generate_audio_for_entry(
     entry,
     delay_seconds,
@@ -146,7 +135,8 @@ def generate_audio_for_entry(
     translation_lang,
     target_gender,
     translation_gender,
-    index
+    index,
+    num_loops
 ):
     word = entry["word"]
     translation = entry["translation"]
@@ -163,12 +153,13 @@ def generate_audio_for_entry(
     )
 
     final_file = (
-        f"{FINAL_DIR}/"
-        f"{index}_"
-        f"{target_lang}_{target_gender}_"
-        f"{translation_lang}_{translation_gender}_"
-        f"{delay_seconds}.wav"
-    )
+    f"{FINAL_DIR}/"
+    f"{index}_"
+    f"{target_lang}_{target_gender}_"
+    f"{translation_lang}_{translation_gender}_"
+    f"{delay_seconds}_"
+    f"{num_loops}.wav"
+)
 
     if os.path.exists(final_file):
         return final_file
@@ -193,21 +184,25 @@ def generate_audio_for_entry(
 
     concatenate_audios(
         [target_file, silence_file, translation_file],
-        final_file
+        final_file,
+        num_loops
     )
 
     return final_file
 
-##############################################################################################################"
 
 
+
+
+######################################## ============================ ################################################################
+########################################             ANTO             ################################################################
+######################################## ============================ ################################################################
 
 engine = create_engine("sqlite:///./vocab.db")
+rows = []
 
 @app.route("/",methods=["GET", "POST"])
 def home():
-
-    rows=None
 
     #initialize database
     conn = sqlite3.connect("vocab.db")
@@ -215,26 +210,34 @@ def home():
     cursor.execute("""
             CREATE TABLE IF NOT EXISTS vocab (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                english TEXT NOT NULL,
-                french TEXT NOT NULL,
+                lang1 TEXT NOT NULL,
+                lang2 TEXT NOT NULL,
                 status INTEGER
             )
             """)
     conn.commit()
 
-   
+   #inizialization of settings
+    pause_duration = float(request.form.get("pause_duration", 2.0))
+    gender_voice = request.form.get("gender_voice", "female")
+    num_loops = int(request.form.get("num_loops", 1))
 
     #insert data into database
     if request.method == "POST":
+
+
+        ####################################
+        #      insert from an exel file    #
+        ####################################
+
         f = request.files.get("file")
         if f:
             file_name=f"{app.config['UPLOAD_FOLDER']}/{f.filename}"
             f.save(file_name)
 
-
             data = pd.read_excel(file_name)
-            data.columns = ["english", "french"]
-            data["status"] = 1
+            data.columns = ["lang1", "lang2"]
+            data["status"] = 1  #is included in the audio by default
             print(data.head())
 
             #initialize database
@@ -247,21 +250,27 @@ def home():
             print("New word", word)
             print("New translation", translation)  
             #new entry
-            new = {"english": word, "french": translation, "status": 1}
+            new = {"lang1": word, "lang2": translation, "status": 1}
             new_data = pd.DataFrame([new])
             #check for duplicates
             existing_data = pd.read_sql("SELECT * FROM vocab", engine)
-            new_data_to_insert = new_data[~new_data['english'].isin(existing_data['english'])]
+            new_data_to_insert = new_data[~new_data['lang1'].isin(existing_data['lang1'])]
             #insert 
             new_data_to_insert.to_sql("vocab", con=engine, if_exists="append", index=False)
 
-        #resultat verif##########################################
+        #resultat verif#####################################################################
         cursor.execute("SELECT * FROM vocab")
         rows = cursor.fetchall()
         for row in rows:
             print(row)
         conn.commit()
-        ####################################################
+        ###################################################################################
+
+
+
+        ####################################
+        #         clear database           #
+        ####################################
 
         clear= request.form.get("clear")
         if clear:
@@ -270,8 +279,12 @@ def home():
             print('database cleared')
             conn.commit()
 
-        update = request.form.get("update")
 
+        ####################################
+        #          upadate status          #
+        ####################################
+
+        update = request.form.get("update")
         if update:
             status_list = []
             for i in range(1, len(rows)+1):
@@ -284,24 +297,31 @@ def home():
             print(status_list)
 
 
-#pause duration setting
-        pause_duration = request.form.get("pause_duration")
-        if pause_duration:
-            pause_duration = float(pause_duration)
-            print(f"Pause duration set to {pause_duration} s")
+        ####################################
+        #     pause duration settings      #
+        ####################################
+        #pause_duration = request.form.get("pause_duration")
+        #if pause_duration:
+            #pause_duration = float(pause_duration)
+            #print(f"Pause duration set to {pause_duration} s")
 
 
-#gender_voice setting
-        gender_voice = request.form.get("gender_voice") 
-        if gender_voice:
-            gender_voice = str(gender_voice)
-            print(f"Gender voice: {gender_voice}")
+        ####################################
+        #      gender_voice setting        #
+        ####################################
+        #gender_voice = request.form.get("gender_voice") 
+        #if gender_voice:
+            #gender_voice = str(gender_voice)
+            #print(f"Gender voice: {gender_voice}")
 
-#num_loops setting
-        num_loops = request.form.get("num_loops")
-        if num_loops:  
-            num_loops = int(num_loops)
-            print(f"Number of loops set to: {num_loops}")
+
+        ####################################
+        #        num_loops setting         #
+        ####################################
+        #num_loops = request.form.get("num_loops")
+        #if num_loops:  
+            #num_loops = int(num_loops)
+            #print(f"Number of loops set to: {num_loops}")
 
 #Language switch setting
 
@@ -316,62 +336,64 @@ def home():
             # ============================
             # Configuration utilisateur
             # ============================
-
             USER_CONFIG = {
                 "target_lang": "cmn-CN",
                 "translation_lang": "en-GB",
-                "target_gender": "female",
-                "translation_gender": "male",
-                "delay_seconds": 2.0
+                "target_gender": gender_voice,
+                "translation_gender": gender_voice,
+                "delay_seconds": pause_duration
             }
-
 
             # ============================
             # Génération audio complet
             # ============================
 
             #Récupération des données depuis la BDD
+            df = pd.read_sql("SELECT lang1 AS word, lang2 AS translation FROM vocab WHERE status=1", engine)
 
-            df = pd.read_sql("SELECT english AS word, french AS translation FROM vocab WHERE status=1", engine)
 
-            #Génération audios complets pour chaque entrée => à adapter pour la BDD
             final_audio_all = AudioSegment.empty()
-            bip = Sine(1000).to_audio_segment(duration=300)
+            for _ in range(num_loops): #nb de repetitions de chaque sequence
+                #Génération audios complets pour chaque entrée => à adapter pour la BDD
+                bip = Sine(1000).to_audio_segment(duration=300)
 
-            for i, (index, row) in enumerate(df.iterrows()):
-                word = row["word"]
-                translation = row["translation"] 
+                for i, (index, row) in enumerate(df.iterrows()):
+                    word = row["word"]
+                    translation = row["translation"] 
+                    
+                    #check if the user has chosen num_loops, if not set to 1
+                    #num_loops = int(request.form.get("num_loops", 1))
 
-                #Audio pour une ligne
-                final_audio_path = generate_audio_for_entry(
-                    entry={
-                        "word": word,
-                        "translation": translation
-                    },
-                    delay_seconds=USER_CONFIG["delay_seconds"],
-                    target_lang=USER_CONFIG["target_lang"],
-                    translation_lang=USER_CONFIG["translation_lang"],
-                    target_gender=USER_CONFIG["target_gender"],
-                    translation_gender=USER_CONFIG["translation_gender"],
-                    index=index + 1
-                )
+                    #Audio pour une ligne
+                    final_audio_path = generate_audio_for_entry(
+                        entry={
+                            "word": word,
+                            "translation": translation
+                        },
+                        delay_seconds=USER_CONFIG["delay_seconds"],
+                        target_lang=USER_CONFIG["target_lang"],
+                        translation_lang=USER_CONFIG["translation_lang"],
+                        target_gender=USER_CONFIG["target_gender"],
+                        translation_gender=USER_CONFIG["translation_gender"],
+                        index=index + 1,
+                        num_loops=num_loops
+                    )
 
-                #Ajout du segment mot à l'audio final
-                final_audio_all += AudioSegment.from_wav(final_audio_path)
+                    #Ajout du segment mot à l'audio final
+                    final_audio_all += AudioSegment.from_wav(final_audio_path)
 
-                #Ajout du bip entre les mots
-                if i < len(df) - 1:
-                    final_audio_all += bip
+                    #Ajout du bip entre les mots
+                    if i < len(df) - 1:
+                        final_audio_all += bip
 
             #Export de l'audio final complet
             final_audio_all.export(
                 f"{FINAL_DIR}/final_output.mp3",
                 format="mp3"
             )
+            print('Audio generation completed. File saved as final_output.mp3')
                                     
-            
-
-
+        #Display database content
         cursor.execute("SELECT * FROM vocab")
         rows = cursor.fetchall()
         conn.close()
@@ -379,9 +401,10 @@ def home():
     
 
 
-    return render_template('index.html', rows=rows)
+    return render_template('index.html', rows=rows, pause_duration=pause_duration, gender_voice=gender_voice, num_loops=num_loops)
 
-#################################################################################################
+
+
 
 
 if __name__ == "__main__":
